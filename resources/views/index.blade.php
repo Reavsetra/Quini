1f71779072b82a00dffabb94cab98005ca768e1a
Emmanuel Navarro <reavsetra@live.com>
2017-05-03 02:02:20 -0500

    creacion

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.principal')
+
+@section('title', 'Bienvenidos')
+
+@section('content')
+    <!-- Anchors (links) -->
+<a href="about.html" class="button">Learn More</a>
+<a href="#features" class="button">View All Features</a>
+
+<!-- Buttons (actions) -->
+<button type="button" class="success button">Save</button>
+<button type="button" class="alert button">Delete</button>
+@endsection
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,5 @@
+<nav>
+    <ul>
+        <li>Inicio</li>
+    </ul>
+</nav>
