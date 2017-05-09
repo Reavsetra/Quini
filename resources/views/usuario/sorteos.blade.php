a35d1389c9d96149619cc7b096e45c10e0d31949
Emmanuel Navarro <reavsetra@live.com>
2017-05-09 14:13:51 -0500

    cambios leves

--- resources/views/usuario/sorteos.blade.php
+++ resources/views/usuario/sorteos.blade.php
@@ -4,4 +4,35 @@
 
 @section('content')
     <h3>Lista de Quinielas Jugadas</h3>
+    <table>
+        <thead>
+            <tr>
+                <th>Quiniela N°</th>
+                <th>Fecha de Compra</th>
+                <th>Combinación</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>004</td>
+                <td>12-05-2017</td>
+                <td><button>Ver</button></td>
+            </tr>
+            <tr>
+                <td>003</td>
+                <td>12-05-2017</td>
+                <td><button>Ver</button></td>
+            </tr>
+            <tr>
+                <td>002</td>
+                <td>12-05-2017</td>
+                <td><button>Ver</button></td>
+            </tr>
+            <tr>
+                <td>001</td>
+                <td>12-05-2017</td>
+                <td><button>Ver</button></td>
+            </tr>
+        </tbody>
+    </table>
 @endsection
